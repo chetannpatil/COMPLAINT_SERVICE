6eb7fa0382070729da16485c6f41452481d38f43
CHETAN <chetu.is@gmail.com>
2020-08-26 14:46:58 +0530

    first commit

---
+++ src/main/java/io/chetan/ComplaintServiceApplication.java
@@ -0,0 +1,13 @@
+package io.chetan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintServiceApplication.class, args);
+	}
+
+}
